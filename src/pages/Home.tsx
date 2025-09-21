eb4a0082d7e759b2325a11ce96469a991b155ad6
Tuvi11 <soujanyabailawad@gmail.com>
2025-09-21 16:33:48 +0530

    changed color of button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -235,7 +235,9 @@ const Home = () => {
       <Button
         asChild
         size="lg"
-        className="bg-white text-[#FF6600] border border-[#FF6600] hover:bg-[#FF6600] hover:text-white transition"
+        className="bg-white text-[#FF6600] border border-[#FF6600] 
+                   hover:bg-gradient-to-r hover:from-[#FF6600] hover:to-[#FFA500] 
+                   hover:text-white transition"
       >
         <Link to="/contact">
           <Phone className="w-5 h-5 mr-2" />
@@ -247,7 +249,9 @@ const Home = () => {
       <Button
         asChild
         size="lg"
-        className="bg-white text-[#FF6600] border border-[#FF6600] hover:bg-[#FF6600] hover:text-white transition"
+        className="bg-white text-[#FF6600] border border-[#FF6600] 
+                   hover:bg-gradient-to-r hover:from-[#FF6600] hover:to-[#FFA500] 
+                   hover:text-white transition"
       >
         <a
           href="https://wa.me/919876543210"
